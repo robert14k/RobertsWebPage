477a2168f4b2d6b01175c41b493f761b932a2a0b
robert14k <robert.kirkpatrick@huskers.unl.edu>
2019-07-20 14:21:25 -0500

    added resume at /Resume.pdf and added anchor but commented it out

--- src/Components/HomeLayout.tsx
+++ src/Components/HomeLayout.tsx
@@ -3,11 +3,14 @@ import React, { Component } from "react";
 import "../Style.css";
 import Background from "../images/mountain-names.jpg";
 //import Lake from '../images/mountainLake.jpg';
-import { Row, Col } from "antd";
+import { Row, Col, Anchor } from "antd";
 import InfoContainer from "./InfoContainer";
+import animateScrollTo from 'animated-scroll-to';
 //import { Grid, Row, Col } from 'react-flexbox-grid';
 
 //  const { Row } = Grid;
+const { Link } = Anchor;
+
 export interface IHomeLayoutProps {
   //none
 }
@@ -30,7 +33,7 @@ export class HomeLayout extends Component<IHomeLayoutProps, IHomeLayoutState> {
     backgroundSize: "cover",
     width: "100%",
     height: "100vh",
-    overflow: "scroll", //this makes it able to scroll the innder component but the backgroudn remains fixed!!!
+    overflow: "scroll" //this makes it able to scroll the innder component but the backgroudn remains fixed!!!
   };
 
   updateDimensions() {
@@ -53,6 +56,13 @@ export class HomeLayout extends Component<IHomeLayoutProps, IHomeLayoutState> {
   componentWillUnmount() {
     window.removeEventListener("resize", this.updateDimensions.bind(this));
   }
+
+  handleClick = (e: any, link: any) => {
+    e.preventDefault();
+    animateScrollTo(link.href);
+    console.log(link);
+  };
+
   // picture=`url(${Headshot})`
   //the height style doesnt work and the classname main header also doesnt work all that well
   mobile: boolean = false;
@@ -66,19 +76,24 @@ export class HomeLayout extends Component<IHomeLayoutProps, IHomeLayoutState> {
         <Row className="headerFont">My name is Robert Kirkpatrick</Row>
         <Row style={{ height: "10px" }} />
         <Row className="subHeaderFont">Software Engineering Student</Row>
+        {/* <Anchor affix={false} onClick={this.handleClick}>
+          <Link href="#about" title="About me" />
+        </Anchor> */}
         <Row style={{ height: "50px" }} />
         <Row style={{ width: "100%" }}>
           {!this.state.mobile && (
             <>
-              <Col span={4}></Col>
+              <Col span={4} />
               <Col span={16}>
                 {/* this is where i want to render my info page, maybe make that a component */}
-                <InfoContainer mobile={this.state.mobile}/>
+                <h3 ref="about">
+                  <InfoContainer mobile={this.state.mobile}/>
+                </h3>
               </Col>
-              <Col span={4}></Col>
+              <Col span={4} />
             </>
           )}
-          {this.state.mobile && <InfoContainer mobile={this.state.mobile}/>}
+          {this.state.mobile && <InfoContainer mobile={this.state.mobile} />}
         </Row>
       </div>
     );
