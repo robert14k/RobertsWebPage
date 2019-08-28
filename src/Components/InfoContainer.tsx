fe3fd809267f44a14f11d2f83c6861adeb1ec54f
Robert Kirkpatrick <robert.kirkpatrick@huskers.unl.edu>
2019-08-28 11:12:29 -0500

    updated about me text

--- src/Components/InfoContainer.tsx
+++ src/Components/InfoContainer.tsx
@@ -85,18 +85,19 @@ export default class InfoContainer extends Component<
               About Me:
             </Row>
             <Row>
-              I’m a sophomore at UNL studying software engineering with minors
-              in business and mathematics. During the school year, I work as a
-              Learning assistant for the computer science II class at UNL which
-              is helping me to better my understanding of the Java programming
-              language. This summer I am a software development intern at
-              Buildertrend in Omaha. <p />I am the secretary and serve on the
-              executive board of Phi Kappa Theta fraternity, an active member of
-              the Newman Center on campus, and an avid Husker football fan. I am
-              excited by new technologies and want to learn about the different
-              disciplines of software and hardware engineering. With my spare
-              time, I play and listen to music, and I am starting to learn and
-              build an application in the JavaScript programming language.
+              I’m a Junior at UNL studying software engineering with minors in
+              business and mathematics. During the school year, I work as a
+              Course Leader for the computer science II class at UNL which is
+              helping me to better my understanding of the Java programming
+              language. This past summer I had an amazing opportunity to be a
+              software development intern at Buildertrend.<p></p>I am the
+              secretary and serve on the executive board of Phi Kappa Theta
+              fraternity, an active member of the Newman Center on campus, and
+              an avid Husker football fan. I am excited by new technologies and
+              want to learn about the different disciplines of software and
+              hardware engineering. With my spare time, I play and listen to
+              music, and I am working on building a full-stack web app with a
+              react.ts front-end and a python and django back-end.
             </Row>
             <Row
               className="bodyHeaderFont"
