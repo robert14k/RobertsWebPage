075a581ba9bdbdb11aba272a402bd6b61842e4ea
robert14k <robert.kirkpatrick@huskers.unl.edu>
2019-10-13 15:03:36 -0500

    got rid of dead code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import React from 'react';
 import "./App.css";
 import 'antd/dist/antd.css';
@@ -9,20 +8,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <HomeLayout/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
