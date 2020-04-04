6860865c25b58f9857dc8550eb923245e164f1e3
Robert Kirkpatrick <robert.kirkpatrick@huskers.unl.edu>
2020-04-04 10:59:58 -0500

    added recent projects and updated about me section

--- src/Components/InfoContainer.tsx
+++ src/Components/InfoContainer.tsx
@@ -5,8 +5,9 @@ import { Row, Card, Avatar, Icon, Col, Divider } from "antd";
 import "../Style.css";
 import webPage from "../images/webPage.png";
 import project156 from "../images/project156.png";
+import reactDjango from "../images/react_django.png";
+import pitivi from "../images/pitivi.png";
 import ProjectEntity from "../Modals/ProjectEntity";
-// import idk from "../images/idk.gif";
 import Project from "./Project";
 export interface IInfoContainerProps {
   mobile: boolean;
@@ -22,7 +23,7 @@ let project1 = new ProjectEntity({
   description:
     "This is my first website created using react and typescript. I decided to create this as a way to learn how to build, deploy, and host a website.",
   link: "https://github.com/robert14k/RobertWebPage",
-  isLeft: false
+  isLeft: false,
 });
 let project2 = new ProjectEntity({
   picture: project156,
@@ -30,19 +31,28 @@ let project2 = new ProjectEntity({
   description:
     "This is a backend application written in Java and SQL to track invoices for a movie theater.",
   link: "https://github.com/robert14k/156Project5.1",
-  isLeft: true
+  isLeft: true,
 });
-// let project3 = new ProjectEntity({
-//   picture: idk,
-//   title: "Python and Kivy application",
-//   description:
-//     "It's in progress, come back later. do i jut need more words? is that it? dats bull shit ",
-//   link: "",
-//   isLeft: false
-// });
-projects[0] = project1;
-projects[1] = project2;
-// projects[2] = project3;
+let project3 = new ProjectEntity({
+  picture: reactDjango,
+  title: "Blog",
+  description:
+    "This is a blog website that I am currently working on. I am creating it to strengthen my web api skills and learn to use django, python, react, and redux.",
+  link: "https://github.com/robert14k/BlogSite",
+  isLeft: false,
+});
+let project4 = new ProjectEntity({
+  picture: pitivi,
+  title: "Pitivi",
+  description:
+    "Pitivi is an open source video editor for Linux. My software class is contributing to Pitivi while learning software development techniques through sprint planning, status meetings, and handoff presentations.",
+  link: "https://gitlab.gnome.org/bracciata/pitivi/-/tree/solid_color_clips",
+  isLeft: true,
+});
+projects[0] = project3;
+projects[2] = project1;
+projects[3] = project2;
+projects[1] = project4;
 
 export default class InfoContainer extends Component<
   IInfoContainerProps,
@@ -65,7 +75,7 @@ export default class InfoContainer extends Component<
             style={{
               height: "auto",
               backgroundColor: "#ECECEC",
-              borderColor: "#ECECEC"
+              borderColor: "#ECECEC",
             }}
           >
             <Row style={{ height: "64px" }} />
@@ -78,7 +88,7 @@ export default class InfoContainer extends Component<
                   height: "20px",
                   width: "20px",
                   color: "#2b3538",
-                  margin: "5px"
+                  margin: "5px",
                 }}
                 type="user"
               />
@@ -87,17 +97,18 @@ export default class InfoContainer extends Component<
             <Row>
               I’m a Junior at UNL studying software engineering with minors in
               business and mathematics. During the school year, I work as a
-              Course Leader for the computer science II class at UNL which is
-              helping me to better my understanding of the Java programming
-              language. This past summer I had an amazing opportunity to be a
-              software development intern at Buildertrend.<p></p>I am the
-              secretary and serve on the executive board of Phi Kappa Theta
-              fraternity, an active member of the Newman Center on campus, and
+              Learning Assistant for the computer science II class at UNL. Being
+              a LA is helping me better my understanding of the java programming
+              language and strengthening my ability to communicate and teach
+              complicated CS principles. This past summer I had an amazing
+              opportunity to be a software development intern at Buildertrend.
+              <p></p>Outside of school, I am a brother of Phi Kappa Theta
+              Fraternity, an active member of the Newman Center on campus, and
               an avid Husker football fan. I am excited by new technologies and
               want to learn about the different disciplines of software and
               hardware engineering. With my spare time, I play and listen to
               music, and I am working on building a full-stack web app with a
-              react.ts front-end and a python and django back-end.
+              react.js front-end and a python and django back-end.
             </Row>
             <Row
               className="bodyHeaderFont"
@@ -108,13 +119,15 @@ export default class InfoContainer extends Component<
             <Row>
               <Project isMobile={this.props.mobile} projects={projects} />
             </Row>
-            <Divider/>
+            <Divider />
             <Row
               className="bodyHeaderFont"
               style={{ height: "40px", textAlign: "center" }}
             >
               <Col span={8}>
-                <a href={"/Resume.pdf"} style={{color: "#2b3538"}}>Resume</a>
+                <a href={"/Resume.pdf"} style={{ color: "#2b3538" }}>
+                  Resume
+                </a>
               </Col>
               <Col span={8}>
                 <a href={"https://github.com/robert14k"}>
@@ -123,20 +136,24 @@ export default class InfoContainer extends Component<
                       height: "20px",
                       width: "20px",
                       color: "#2b3538",
-                      margin: "5px"
+                      margin: "5px",
                     }}
                     type="github"
                   />
                 </a>
               </Col>
               <Col span={8}>
-                <a href={"https://www.linkedin.com/in/robert-kirkpatrick-79282916a/"}>
+                <a
+                  href={
+                    "https://www.linkedin.com/in/robert-kirkpatrick-79282916a/"
+                  }
+                >
                   <Icon
                     style={{
                       height: "20px",
                       width: "20px",
                       color: "#2b3538",
-                      margin: "5px"
+                      margin: "5px",
                     }}
                     type="linkedin"
                   />
@@ -144,7 +161,7 @@ export default class InfoContainer extends Component<
               </Col>
             </Row>
           </Card>
-          <Row className="footerFont">Created By: Robert Kirkpatrick</Row>
+          <Row className="footerFont">Created By: Robert Kirkpatrick ©</Row>
         </div>
       </>
     );
